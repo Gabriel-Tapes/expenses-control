477af04944481f86eaa9da91d9b501a26c525958
gabriel-tapes <gabrielsouza.iacs@gmail.com>
2023-08-21 21:53:35 -0300

    add in memory users repository

---
+++ src/repositories/inMemory/inMemoryUsersRepository.ts
@@ -0,0 +1,53 @@
+import { User } from '@/models/user'
+import { IUsersRepository } from '../IUsersRepository'
+
+export class InMemoryUsersRepository implements IUsersRepository {
+  private data: User[]
+
+  constructor() {
+    this.data = []
+  }
+  async createUser(user: User): Promise<void> {
+    this.data.push(user)
+  }
+  async getUserById(id: string): Promise<User | null> {
+    const index = this.data.findIndex(user => user.id === id)
+
+    return this.data[index]
+  }
+  async getUserByEmail(email: string): Promise<User | null> {
+    const index = this.data.findIndex(user => user.email === email)
+
+    return this.data[index]
+  }
+  async editUser({
+    id,
+    name,
+    lastName,
+    password
+  }: {
+    id: string
+    name?: string | undefined
+    lastName?: string | undefined
+    password?: string | undefined
+  }): Promise<User | null> {
+    const index = this.data.findIndex(user => user.id === id)
+
+    if (index === -1) return null
+
+    this.data[index].name = name ?? this.data[index].name
+    this.data[index].lastName = lastName ?? this.data[index].lastName
+    this.data[index].password = password ?? this.data[index].password
+
+    return this.data[index]
+  }
+  async deleteUser(userId: string): Promise<number> {
+    const initialLength = this.data.length
+
+    this.data = this.data.filter(user => user.id !== userId)
+
+    if (this.data.length > initialLength) return 1
+
+    return 0
+  }
+}
